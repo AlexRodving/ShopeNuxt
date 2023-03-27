a1edfed70777c0fb27e43f6c0f9c6cfe69241dfc
AlexRodving <alexrodving@gmail.com>
2023-03-27 23:24:36 +0300

    del import defineProps

--- components/CategoriesCard.vue
+++ components/CategoriesCard.vue
@@ -10,7 +10,6 @@
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
 const { cat } = defineProps(['cat'])
 </script>
 
--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -15,7 +15,6 @@
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
 const basketStore = useBasketStore();
 const { product } = defineProps(['product'])
 </script>
